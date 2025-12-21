df4e010a88eca4b726d2c992a2f6eb5aef256be7
Brian <mulindijrn@gmail.com>
2025-12-21 11:56:30 +0300

    php multidimensional arrays

---
+++ arrays/multidimensional_arrays.php
@@ -0,0 +1,23 @@
+<?php 
+
+$rooms = [
+  [
+    "type" => "Single",
+    "price" => 3000,
+    "available" => true
+  ],
+  [
+    "type" => "Suite",
+    "price" => 9000,
+    "available" => false
+  ]
+];
+
+foreach($rooms as $room) {
+  echo "Room Type: " . $room["type"] . PHP_EOL;
+  echo "Price: " . $room["price"] . PHP_EOL;
+  echo "Available: " . ($room["available"] ? "Yes" : "No") . PHP_EOL;
+  echo "------------------------" . PHP_EOL;
+}
+
+?>
