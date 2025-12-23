2d68591cd633ff8a67008df1e2d7914d16f9d2e1
Brian <mulindijrn@gmail.com>
2025-12-23 09:41:04 +0300

    php indexed arrays

---
+++ arrays/indexed_arrays.php
@@ -0,0 +1,10 @@
+<?php 
+
+$roomTypes = ["single", "double", "suite", "deluxe"];
+
+echo $roomTypes[0] . PHP_EOL;
+echo $roomTypes[1] . PHP_EOL;
+echo $roomTypes[2] . PHP_EOL;
+echo $roomTypes[3] . PHP_EOL;
+
+?>
